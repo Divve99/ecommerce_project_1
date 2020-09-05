2e40b572a700cda19a8556d69ea66105b7b5a074
Aparna <aparnanaidu20@yahoo.com>
2020-09-05 10:26:44 +0300

    Task 3 .fs file file added
    javascript things will be added to the cart

--- ecommerce/static/js/cart.js
+++ ecommerce/static/js/cart.js
@@ -36,6 +36,28 @@ function addCookieItem(productId, action){
     console.log('Cart:', cart)
     document.cookie = 'cart=' + JSON.stringify(cart) + ";domain=;path=/"
 }
+function addCookieItem(productId, action){
+    console.log(' Not logged in... ')
+
+    if (action == 'add'){
+        if (cart[productId] == undefined){
+            cart[productId] = {'quantity': 1}
+        }else{
+            cart[productId]['quantity'] += 1
+        }
+    }
+
+    if (action == 'remove'){
+        cart[productId]['quantity'] -= 1
+
+        if (cart[productId]['quantity'] <= 0){
+            console.log(' Item should be deleted ')
+            delete cart[productId]
+        }
+    }
+    console.log('Cart:', cart)
+    document.cookie = 'cart=' + JSON.stringify(cart) + ";domain=;path=/"
+}
 
 
 function updateUserOrder(productId, action){
